7d13c7c908dbe5615ab39b153d104577fb3844af
philnhex3 <37580308+philnhex3@users.noreply.github.com>
2018-04-10 17:53:14 +0200

    new

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,10 +15,10 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
