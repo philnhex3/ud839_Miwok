ba3eb1abe151c46c5cd3202cbd9f1fe667bbea37
philnhex3 <37580308+philnhex3@users.noreply.github.com>
2018-04-12 19:29:41 +0200

    New

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -11,5 +11,21 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers2);
+
+        //Created the array of numbers
+        String [] numbers = new String [10];
+
+        //Assign the values
+        numbers[0] = "one";
+        numbers[1] = "two";
+        numbers[2] = "three";
+        numbers[3] = "four";
+        numbers[4] = "five";
+        numbers[5] = "six";
+        numbers[6] = "seven";
+        numbers[7] = "eight";
+        numbers[8] = "nine";
+        numbers[9] = "ten";
+        
     }
 }
